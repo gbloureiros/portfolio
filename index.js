035e7cdfc5b87cf85eef9a6c19361f77ef551b4b
Gabriel <gloureiros2004@gmail.com>
2024-12-01 03:57:44 -0300

    melhorando o dark mode e mudando o footer

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+const lightIcon = document.getElementById("light-icon");
+const darkIcon = document.getElementById("dark-icon");
 const $html = document.querySelector("html");
-const $checkbox = document.querySelector("#switch");
 
-$checkbox.addEventListener("change", function () {
-  $html.classList.toggle("dark-mode");
-});
+const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+let darkMode = darkModeMediaQuery.matches;
+
+if (darkMode) {
+  document.body.classList.add("dark-mode");
+  darkIcon.setAttribute("display", "none");
+} else {
+  lightIcon.setAttribute("display", "none");
+}
+
+function toggleDarkMode() {
+    darkMode = !darkMode;
+
+    $html.classList.toggle("dark-mode");
+  
+    if (darkMode) {
+      lightIcon.setAttribute("display", "block");
+      darkIcon.setAttribute("display", "none");
+    } else {
+      lightIcon.setAttribute("display", "none");
+      darkIcon.setAttribute("display", "block");
+    }
+}
