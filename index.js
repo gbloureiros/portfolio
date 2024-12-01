c35ce36bcb7f3329aab1574c4b9ed5d3e7100bb9
Gabriel <gloureiros2004@gmail.com>
2024-12-01 19:18:57 -0300

    adicionando coisas novas com javascript

--- index.js
+++ index.js
@@ -25,3 +25,46 @@ function toggleDarkMode() {
       darkIcon.setAttribute("display", "block");
     }
 }
+
+var typed = new Typed('.auto-type', {
+  strings: ['Developer'],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true
+});
+
+let links = document.querySelectorAll('.js-link');
+let sections = document.querySelectorAll('.section')
+
+window.addEventListener('scroll', () => {
+  sections.forEach(section => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 67;
+    let heightSection = section.offsetHeight;
+    let idSection = section.getAttribute('id');
+    
+    if (top >= offset && top < offset + heightSection) {
+      links.forEach(link => {
+        link.classList.remove('actived');
+
+        document.querySelector(`.header .navegacao ul li a[href*='${idSection}']`).classList.add('actived');
+      })
+    }
+  })
+});
+
+function scrollSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+  let topSection = section.offsetTop - 67;
+
+  window.scrollTo({
+    top: topSection,
+    behavior: 'smooth'
+  })
+};
+
+links.forEach(link => {
+  link.addEventListener('click', scrollSection)
+});
